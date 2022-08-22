f5bfe1e1d7e2daad11be1770b35ff2c430ae28e4
anlishamaharjan <anlishamaharjan@gmail.com>
2022-08-22 17:24:25 +0545

    feat: add svg icons

---
+++ src/components/svg/index.js
@@ -0,0 +1,5 @@
+export { default as IconInfo } from "./icon-info";
+export { default as IconAdd } from "./icon-add";
+export { default as IconTrash } from "./icon-trash";
+export { default as IconClock } from "./icon-clock";
+export { default as IconRoute } from "./icon-route";
